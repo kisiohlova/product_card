5e1fed6ddb5c1c04e6a23acaba855096ecd78666
kisiohlova <kisioglovan@gmail.com>
2024-01-21 20:45:17 +0200

    add scraper service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @url = params[:url]
+    @scraped_data = scrape_data if @url.present?
+  end
+
+  private
+
+  def scrape_data
+    scraper_service = ScraperService.new(@url)
+    scraper_service.scrape_data
   end
 end
